ae5b5afee0b610fa1e498d94702d2929d7d3a4e7
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2024-05-18 10:49:55 +0530

    Recursive : final code for recursive basics

--- recursive.js
+++ recursive.js
@@ -8,12 +8,15 @@ let n = 10;
 for (let ind = 1; ind <= n; ind++) {
   //   console.log(ind); // 1 2 3 4 5 6 7 8 9 10
 }
-
-//recursive approach
-function printTill(n, ind) {
-  if (ind > n) return false;
-  console.log(ind); // 1 2 3 4 5 6 7 8 9 10
-  printTill(n, ind + 1);
-  console.log(ind); // 10 9 8 7 6 5 4 3 2 1  // this print while return step by step this is how recursive work
+for (let ind = n; ind > 0; ind--) {
+  //   console.log(ind); // 1 2 3 4 5 6 7 8 9 10
 }
-printTill(10, 1);
+
+// //recursive approach
+// function printTill(n, ind) {
+//   if (ind > n) return false;
+//   console.log(ind); // 1 2 3 4 5 6 7 8 9 10
+//   printTill(n, ind + 1);
+//   console.log(ind); // 10 9 8 7 6 5 4 3 2 1  // this print while return step by step this is how recursive work
+// }
+// printTill(10, 1);
