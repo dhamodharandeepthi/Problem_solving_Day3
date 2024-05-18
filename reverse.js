0e90118f97971f837db71de7d3098ebdc4e8ed9a
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2024-05-18 20:09:42 +0530

    ReverseString

--- reverse.js
+++ reverse.js
@@ -1,15 +1,12 @@
 //Reverse a given string
 let str = "dhamu";
-str = str.split("");
-for (let start = 0, end = str.length - 1; start < end; start++, end--) {
-  let char = str[start];
-  str[start] = str[end];
-  str[end] = char;
-}
-console.log(str); //[ 'u', 'm', 'a', 'h', 'd' ]
-console.log(str.join("")); //umahd
 
-/*
-note:
-strings are immutable so couldn't swap so change to array using split function then join 
-*/
+function helper(str, start) {
+  if (str.length == start) return "";
+  return helper(str, start + 1) + str[start];
+}
+function reverse(str) {
+  return helper(str, 0);
+}
+
+console.log(reverse(str)); //umahd
