06d45c2aaf32e8ca30faa4e47388f232848b493d
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2024-05-18 18:58:37 +0530

    ReverseString : code added using iteration

---
+++ reverse.js
@@ -0,0 +1,8 @@
+//Reverse a given string
+let str = "dhamu";
+let reversedStr = "";
+for (let i = str.length - 1; i >= 0; i--) {
+  reversedStr += str[i];
+}
+console.log(str); //dhamu
+console.log(reversedStr); //umahd
