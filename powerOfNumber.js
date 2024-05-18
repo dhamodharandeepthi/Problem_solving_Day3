19f7f67383e821a90dc7736d228d1fe30724c9f1
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2024-05-18 15:50:44 +0530

    PowerOfNumber : final optimized code satisfied all condition

--- powerOfNumber.js
+++ powerOfNumber.js
@@ -6,15 +6,17 @@ num2=5;
 2**4=>2*2*2*2 =>2*(2**3)   num1 * num2-1
 2**3=>2*2*2 =>2*(2**2)     num1 * num2-1
 2**2=>2*2 =>2*(2**1)       num1 * num2-1
-2**1=>2         =>num2=1 return 2
+2**1=>2         =>num2=1 return num1
 2**0=>1         =>num2=0 return 1
 */
 
 function powerOfNumber(num1, num2) {
+  if (num2 < 0) return 1 / powerOfNumber(num1, -num2);
   if (num2 == 0) return 1;
-  if (num2 == 1) return 2;
+  if (num2 == 1) return num1;
   return num1 * powerOfNumber(num1, num2 - 1);
 }
 let num1 = 2;
 let num2 = 5;
-console.log("2**5", powerOfNumber(num1, num2));
+console.log("2**5: ", powerOfNumber(num1, num2)); //32
+console.log("10**-2: ", powerOfNumber(10, -2)); // 0.01
